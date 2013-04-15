266d6075bfdc3f8b3827049fafee86eb9955be3e
Marvin Qian <fueledbymarvin@gmail.com>
2013-04-15 10:31:52 -0400

    Changed event display order on index.

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -5,7 +5,8 @@ class EventsController < ApplicationController
   # GET /events
   # GET /events.json
   def index
-    @events = Event.all(:order => 'created_at DESC', :limit => 5)
+    newsdate = Date.today;
+    @events = Event.find(:all, :conditions => ["newsuntil >= ?", newsdate], :order => "priority DESC")
 
     respond_to do |format|
       format.html # index.html.erb
