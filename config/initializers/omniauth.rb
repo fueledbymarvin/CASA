ec08debd2cafc23bc9e49988d123694dbfc366da
Marvin Qian <fueledbymarvin@gmail.com>
2013-12-30 16:33:27 -0500

    Move Facebook app info

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,11 +1,3 @@
-if Rails.env == "production"
-	ENV['fb_app_id'] = '649802481704656'
-	ENV['fb_secret'] = 'c59bd62b1863bf443cae32ba3306151c'
-else
-	ENV['fb_app_id'] = '343629322437383'
-	ENV['fb_secret'] = '7e510c5b72f620481bf44d5873914161'
-end
-
 ENV['fb_permissions'] = "create_event, user_groups"
 
 Rails.application.config.middleware.use OmniAuth::Builder do
